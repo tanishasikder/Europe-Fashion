3978d2efb32a99b0237cacf0c60d9c5fef878fdc
tanisha <thetanishasikder@gmail.com>
2025-10-28 20:41:57 -0400

    Made separate classifiers for color and type

--- Business Logic/image_extraction.py
+++ Business Logic/image_extraction.py
@@ -36,35 +36,58 @@ data_transforms = {
 }
 
 data_dir = 'Europe-Fashion/Fashion_images'
-sets = ['train', 'val']
+sets = ['color_train', 'type_train', 'color_val', 'type_val']
 
 # Getting the data based on the train/val sets then doing transformations
 image_datasets = {x : datasets.ImageFolder(os.path.join(data_dir, x),
                                            data_transforms[x])
                                            for x in sets}
 
-# Loading the data in batches
+# Loading the data in batches. Separate dataloaders for color and type tests
 data_loaders = {
-    'train' : DataLoader(image_datasets['train'], batch_size=32, shuffle=True, num_workers=4, pin_memory=True),
-    'val' : DataLoader(image_datasets['val'], batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
-
+    'color_train' : DataLoader(image_datasets['color_train'], batch_size=32, 
+                               shuffle=True, num_workers=4, pin_memory=True),
+    'type_train' : DataLoader(image_datasets['type_train'], batch_size=32, 
+                              shuffle=True, num_workers=4, pin_memory=True),
+    'color_val' : DataLoader(image_datasets['color_val'], batch_size=32, 
+                             shuffle=False, num_workers=4, pin_memory=True),                          
+    'type_val' : DataLoader(image_datasets['type_val'], batch_size=32, 
+                            shuffle=False, num_workers=4, pin_memory=True)
 }
 
 dataset_sizes = {x : len(image_datasets[x]) for x in sets}
-class_names = image_datasets['train'].classes
+color_names = image_datasets['color_train'].classes
+type_names = image_datasets['type_train'].classes
 
-# Load in the pretrained resnet model
-model = models.resnet18(pretrained=True)
+# CNN class to classify image features
+class CNN(nn.Module):
+    def __init__(self, color_names, type_names, device):
+        super().__init()
+        # Load in the pretrained resnet model
+        model = models.resnet18(pretrained=True)
+        self.resnet = model
 
-for param in model.parameters():
-    # Freeze all the layers in the beginning
-    param.requires_grad = False
+        # Freeze parameters
+        for param in self.resnet.parameters():
+            param.requires_grad = False
 
-# Finding the number of input neurons from the resnet model
-num_features = model.fc.in_features
-model.fc = nn.Linear(num_features, len(class_names))
-model.to(device)
+        # Assign a fully connected layer containing the class names
+        num_features = self.resnet.fc.in_features
+        # Head to classify the color
+        self.fc_color = nn.Linear(num_features, len(color_names))
+        # Head to classify the clothing type
+        self.fc_type = nn.Linear(num_features, len(type_names))
+        self.resnet.to(device)
+    
+    def forward(self, x):
+        # Gather features and assign it to the color and type heads
+        features = self.resnet.features(x)
+        color = self.fc_color(features)
+        type = self.fc_type(features)
+        # Return the classification
+        return color, type
 
+model = CNN(color_names, type_names, device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001)
 
@@ -79,10 +102,9 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=None):
 
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs - 1}')
-
         # Switch between training and validation
         for phase in sets:
-            if phase == 'train':
+            if phase == 'color_train' or 'type_train':
                 model.train()
             else:
                 model.eval()
@@ -91,20 +113,26 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=None):
         correct = 0
 
         # Loop over the labels and the images in the dataloader
-        # Changes between train and val
+        # Changes between train/val/color/type
         for input, label in data_loaders[phase]:
             inputs = input.to(device)
             labels = label.to(device)
 
             with torch.set_grad_enabled(phase=='train'):
                 # Gets the outputs from resnet model
-                outputs = model(inputs)
+                output = None
+                color, type = model(inputs)
+                track = phase.replace('_train', '')
+                if track == 'color': 
+                    output = color 
+                else: 
+                    output = type
                 # Gets the largest score then calculates loss
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
+                _, preds = torch.max(output, 1)
+                loss = criterion(output, labels)
 
                 # Optimizes and backward propagates if it is training
-                if phase == 'train':
+                if phase in ['color_train','type_train']:
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
@@ -113,7 +141,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=None):
             run_loss += loss.item() * inputs.size(0)
             correct += torch.sum(preds == labels.data)
 
-        if phase == 'train':
+        if phase in ['color_train', 'type_train']:
             scheduler.step()
         
         # Overall loss and accuracy of this model
@@ -123,7 +151,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=None):
         print(f'{phase} Loss : {epoch_loss:.3f} Accuracy : {epoch_accuracy:.3f}')
 
         # If it is validation, find the best model by finding the best accuracy
-        if phase == 'val' and epoch_accuracy > best_accuracy:
+        if phase in ['color_val', 'type_val'] and epoch_accuracy > best_accuracy:
             best_accuracy = epoch_accuracy
             best_model = copy.deepcopy(model.state_dict())
 
