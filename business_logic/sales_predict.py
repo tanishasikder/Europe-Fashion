Fixed data leakage issues

business_logic/sales_predict.py
@@ -11,6 +11,7 @@ import mlflow
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import OneHotEncoder
 
 # Creating a class for MTGBM with parameters to inherit.
 # BaseEstimator and RegressorMixin gives methods
@@ -224,6 +225,8 @@ class MTGBM(BaseEstimator, RegressorMixin):
         #Adds a column to show profit margin
         product_compare['profit_margin'] = (product_compare['unit_price'] - product_compare['cost_price']) / product_compare['unit_price']
         #product_compare = product_compare[product_compare['profit_margin'] >= 0]
+        
+        '''
         # Categorical variables for encoding
         categorical = ['category', 'color', 'size', 'channel']
 
@@ -235,9 +238,10 @@ class MTGBM(BaseEstimator, RegressorMixin):
             product_compare[col] = label.fit_transform(product_compare[col])
             # Store the encoders used for every categorical variable
             categorical_encoding[col] = label
+        '''
+        X = product_compare[['category', 'color', 'size', 'channel', 'catalog_price', 'original_price', 'unit_price', 'cost_price']]
+        y = product_compare[['profit_margin', 'quantity', 'profit']]
 
-        X = product_compare[['category', 'color', 'size', 'catalog_price', 'channel', 'original_price', 'unit_price', 'cost_price']].values
-        y = product_compare[['profit_margin', 'quantity', 'profit']].values
         
         '''
         X_train, X_test, y_train, y_test = train_test_split(
@@ -246,15 +250,13 @@ class MTGBM(BaseEstimator, RegressorMixin):
         '''
         cv = LeaveOneOut()
 
-        for train_index, test_index in cv.split(X):
+        # Categorical variables for encoding
+        categorical = ['category', 'color', 'size', 'channel']
+        numerical = ['catalog_price', 'original_price', 'unit_price', 'cost_price']
 
-            X_train, X_test = X[train_index, :], X[test_index, :]
-            y_train, y_test = y[train_index], y[test_index]
-            # Profit margin, quantity, and item total are on different scales
-            scaler = StandardScaler()
-            
-            y_train = scaler.fit_transform(y_train)
-            y_test = scaler.transform(y_test)
+        for train_index, test_index in cv.split(X):
+            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
             # These indices skew the data negatively
             mask = np.ones(len(X_train), dtype=bool)
@@ -262,7 +264,26 @@ class MTGBM(BaseEstimator, RegressorMixin):
             y_train = y_train[mask]
             X_train = X_train[mask]
 
-            #['category', 'color', 'size', 'catalog_price', 'channel', 'original_price', 'unit_price']
+            one_hot = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+            
+            categorical_X_train = one_hot.fit_transform(X_train[categorical])
+            categorical_X_test = one_hot.transform(X_test[categorical])
+
+            # Profit margin, quantity, and item total are on different scales
+            X_scaler = StandardScaler()
+            y_scaler = StandardScaler()
+
+            num_X_train = X_scaler.fit_transform(X_train[numerical])
+            num_X_test = X_scaler.transform(X_test[numerical])
+
+            X_train = np.hstack([categorical_X_train, num_X_train])
+            X_test = np.hstack([categorical_X_test, num_X_test])
+
+            y_train = y_scaler.fit_transform(y_train)
+            y_test = y_scaler.transform(y_test)
+            # new ['category', 'color', 'size', 'channel', 'catalog_price', 'original_price', 'unit_price', 'cost_price']
+            # old ['category', 'color', 'size', 'catalog_price', 'channel', 'original_price', 'unit_price', 'cost_price']
+
             # Predicting profit margin for the first test
             # Predict how much quantity bought will change if unit_price changes
             # Predict item total based on original price and channel
@@ -291,7 +312,7 @@ class MTGBM(BaseEstimator, RegressorMixin):
 
                 # Make the model predict then test the metrics
                 pred = model.predict_values(X_test)
-                real_pred = scaler.inverse_transform(pred)
+                #real_pred = X_scaler.inverse_transform(pred)
 
                 #print(f"TRAIN set size: {len(train_index)}")
                 #print(f"TEST set size: {len(test_index)}")
@@ -306,7 +327,7 @@ class MTGBM(BaseEstimator, RegressorMixin):
 
                 '''
                 for i, name in enumerate(['profit_margin', 'quantity', 'profit']):
-                    mse = mean_squared_error(y_test[:, i], real_pred[:, i])
+                    mse = mean_squared_error(y_test[:, i], pred[:, i])
                     print(f"{name}: {mse}")
                 #print(mse)
 
@@ -314,7 +335,7 @@ class MTGBM(BaseEstimator, RegressorMixin):
                 # Log metrics and model
                 #mlflow.log_metric("mse", mse)
                 #mlflow.sklearn.log_model(model, "sales_predict_model")
-        return model
+        #return model
 
 mtgbm = MTGBM()
 mtgbm.train_model()