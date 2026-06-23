c7ed131b3d9bd160467962f125f9d9cec2108738
tanisha <thetanishasikder@gmail.com>
2026-06-22 22:31:02 -0400

    Fixed errors and organized files

--- testing purposes/sales_pred.py
+++ testing purposes/sales_pred.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+import lightgbm as lgb
+from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.preprocessing import PowerTransformer
 from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import train_test_split, cross_val_score
+from typing import List, Dict
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import mlflow
 from sklearn.preprocessing import StandardScaler
@@ -15,12 +19,27 @@ categorical = ['category', 'color', 'size', 'channel']
 numerical = ['catalog_price', 'original_price', 'unit_price', 'cost_price']
 
 def initialization():
-    data = pd.read_excel('data/Fashion Data/DataPenjualanFashion.xlsx', sheet_name=None)
+    data = pd.read_excel('Fashion Data/DataPenjualanFashion.xlsx', sheet_name=None)
 
     # Separating the data into dataframes
     products = data['ProductItems']
     sales_items = data['SalesItems']
     pivot = data['Pivot Table']
+
+    # Cleaning the data
+    # Reorganizing pivot
+    channel_data = {'Channels' : ['App Mobile', 'E-commerce'],
+                    'Totals Of Original Price' : [53952.79, 57167.84]}
+
+    types_data = {'Type Product' : ['Dresses', 'Pants', 'Shoes', 'Sleepwear', 'T-Shirt', 'Grand Total'],
+            'Total Catalog Price' : [5298.44, 3959.36, 5236.03, 4933.21, 5511.98, 24939.02], 
+            'Total Cost Price' : [2917.77, 2149.76, 2908.44, 2785.75, 2962.69, 13724.41]}
+
+    # Shows the money by the channels
+    channel = pd.DataFrame(channel_data)
+
+    # Shows each category of clothing and their listed price and cost to make
+    types = pd.DataFrame(types_data)
 
     pivot = pivot.dropna()
     pivot = pivot.drop(19)
@@ -35,6 +54,16 @@ def initialization():
         left_index=True,
         right_index=True
     )
+
+    # Save product_compare as a CSV file to generate synthetic data
+    #product_compare.to_csv('C:/Users/Tanis/Downloads/Europe-Fashion/Fashion Data/product_comparing.csv', index=False)
+
+    # After making the CSV file, generate synthetic data, loading here.
+    #synthetic = pd.read_csv('Fashion Data/synthetic_data.csv')
+    
+    #synthetic_sample = synthetic.sample(n=100, random_state=42)
+    #Combine synthetic data with original data
+    #product_compare = pd.concat([product_compare, synthetic_sample], ignore_index=True)
 
     # Adds a column to show the product's cost to make
     product_compare['cost_to_make'] = product_compare['cost_price'] * product_compare['quantity']
@@ -76,7 +105,7 @@ def train_model(X, y):
     c = [col for col in categorical if col in X.columns]
     n = [col for col in numerical if col in X.columns]
 
-    greatest_mse = 0.0
+    average_mse = 0.0
     for train_index, test_index in cv.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -108,9 +137,16 @@ def train_model(X, y):
 
         with mlflow.start_run():
             model = SVR(kernel='rbf', C=1.0, gamma='scale')
+            model.fit(X_train, y_train)
+            mlflow.log_params(model.get_params())
+            #DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            #path = os.path.join(DIR, "business_logic", "stats_model.joblib")
+            #dump(model, path)
+
+            model = SVR(kernel='rbf', C=1.0, gamma='scale')
 
             model.fit(X_train, y_train.ravel())
-            mlflow.log_params(model.get_params())
+
             y_pred = model.predict(X_test)
 
             # Bring it back to the same dimension
@@ -118,19 +154,37 @@ def train_model(X, y):
             real_y_test = yeo_johnson.inverse_transform(y_test)
 
             mse = mean_squared_error(real_y_test, real_pred)
+            #mse = mean_squared_error(y_test, y_pred)
             print(mse)
             print(f'pred: {y_pred}')
+            #print((average_mse += mse) / 500)
+            #print(f"TRAIN set size: {len(train_index)}")
+            #print(f"TEST set size: {len(test_index)}")
+            #print(f"mse: {mean_squared_error(y_test, real_pred)}\n")
 
-            if mse > greatest_mse:
-                # Log metrics and model
-                mlflow.log_metric("mse", mse)
-                mlflow.sklearn.log_model(model, "sales_predict_model")
+            '''
+            # TO DO: AFTER GETTING QUANTITY PREDICT WITH THE CATALOG PRICE GIVEN IN CUSTOMER_PREDICT
+            quantites = real_pred[:, 1]
+
+            for i, j in zip(quantites, X_test[:, 3]):
+                print(i * j)
+
+            
+            for i, name in enumerate(['profit_margin', 'quantity', 'profit']):
+                if name == 'profit_margin':
+                    print(f"{name}: {mean_squared_error(real_y_test[:, i], real_pred[:, i])}")
+                else:
+                    print(f"{name}: {mean_absolute_error(real_y_test[:, i], real_pred[:, i])}")
+            #print(mse)
+
+            '''
+            # Log metrics and model
+            mlflow.log_metric("mse", mse)
+            mlflow.sklearn.log_model(model, "sales_predict_model")
+    #return model
 
 X, y = initialization()
 
 for num in range(3):
     features, predicts = split(X, y, num)
     train_model(features, predicts)
-
-
-
