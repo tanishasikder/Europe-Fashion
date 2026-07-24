13bb21f993f34d36641bc0d3161fdcb976c4a9ca
tanisha <thetanishasikder@gmail.com>
2026-07-23 21:12:28 -0400

    Doing more of color extraction

--- src/models/color_extract.py
+++ src/models/color_extract.py
@@ -4,26 +4,50 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from PIL import Image
 from sklearn.metrics.pairwise import euclidean_distances
+from pathlib import Path
 
-#Read in image and convert to RGB
-img = Image.open('data/Fashion_Images/train/black_dress/download (1).jpg').convert('RGB')
+def read_in(file):
+    #Read in image and convert to RGB
+    img = Image.open(file).convert('RGB')
 
-# Image transformations, resize image
-resized = img.resize((50, 200))
+def preprocess(img):
+    # Image transformations, resize image
+    resized = img.resize((50, 200))
 
-arr = np.array(resized)
-float = arr.astype(np.float32) / 255.0
+    arr = np.array(resized)
+    float = arr.astype(np.float32) / 255.0
 
-h, w, c, = float.shape # Saving the original shape
-pixels = float.reshape(-1, c)  # Making it 2d dimensions for KMeans
+    h, w, c = float.shape # Saving the original shape
+    pixels = float.reshape(-1, c)  # Making it 2d dimensions for KMeans
 
-kmeans = KMeans(n_clusters=10, random_state=42, n_init="auto")
-kmeans.fit(pixels)
+    return pixels, (h, w, c)
 
-labels = kmeans.labels_  # Cluster assignment for each point
-centroids = kmeans.cluster_centers_  # Coordinates of cluster centers
+def fit_model(pixels, dimen):
+    h, w, c, = dimen
+    kmeans = KMeans(n_clusters=10, random_state=42, n_init="auto")
+    kmeans.fit(pixels)
 
-segmented_pixels = centroids[labels]
-segmented_img = segmented_pixels.reshape(h, w, c)
+    labels = kmeans.labels_  # Cluster assignment for each point
+    centroids = kmeans.cluster_centers_  # Coordinates of cluster centers
 
+    segmented_pixels = centroids[labels]
+    segmented_img = segmented_pixels.reshape(h, w, c)
 
+def list_files():
+    # Get directory of current folder
+    current_dir = Path(__file__).resolve().parent
+
+    # Go up top-most folder
+    root_dir = current_dir.parents[1]
+
+    train_path = root_dir / 'data' / 'Fashion_Images' / 'train'
+    val_path = root_dir / 'data' / 'Fashion_Images' / 'val'
+
+    train_files = [item.name for item in train_path.iterdir()]
+    val_files = [item.name for item in val_path.iterdir()]
+
+    print(train_files)
+    print(val_files)
+
+list_files()
+    
