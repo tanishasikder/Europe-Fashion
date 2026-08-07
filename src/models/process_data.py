41106c6d381d5d32e778bd91524e9c69dad88b8a
tanisha <thetanishasikder@gmail.com>
2026-08-06 20:33:54 -0400

    Making progress with models class

--- src/models/process_data.py
+++ src/models/process_data.py
@@ -14,7 +14,6 @@ import dagshub
 import numpy as np
 from PIL import Image
 from pathlib import Path
-import sys
 from dotenv import load_dotenv
 from torch.utils.data import Dataset
 from torch.utils.data import random_split
@@ -27,7 +26,7 @@ std = np.array([0.229, 0.224, 0.225])
 
 color_dir = os.environ.get('COLOR_DIR')
 
-def fashion_transform(data):
+def fashion_transform():
     # Fashion images have bigger transformations
     fashion_transforms = {
         'train' : transforms.Compose([
@@ -45,9 +44,9 @@ def fashion_transform(data):
             transforms.Normalize(mean, std)
         ])
     }
-    return fashion_transforms[data]
+    return fashion_transforms
 
-def color_transform(data):
+def color_transform():
     color_transforms = {
         'train' : transforms.Compose([
             transforms.Resize(256),
@@ -60,28 +59,30 @@ def color_transform(data):
             transforms.Normalize(mean, std)
         ])
     }
-    return color_transforms[data]
+    return color_transforms
 
 def process_colors():
     color_dict = {}
     paths = []
 
     for path, mid, file in os.walk(color_dir):
-        paths.append(path) # For opening the image later one
+        paths.append(path) # For opening the image later on. Need full path
         marker = path.find('colors\\')
         path = path[marker+7:]
+        slash = path.find('\\')
 
-        color_dict[path] = file
+        if slash != -1:
+            color_dict[path] = file
 
-    return color_dict
+    # Remove first value its the file path to whole folder
+    (k := next(iter(color_dict)), color_dict.pop(k))
 
-dict = process_colors()    
-print(dict.keys())
+    return color_dict, paths
 
 class ColorData(Dataset):
     def __init__(self, data, paths, transform):
         self.data = data
-        self.colors = []
+        #self.colors = []
         self.paths = paths
         self.transform = transform
 
@@ -89,7 +90,7 @@ class ColorData(Dataset):
         return len(data)
 
     def encoding(self, name):
-        codes = {
+        codes = { # Pytorch datasets encode labels with data
             'Black' : 0,
             'Blue' : 1,
             'Gray' : 2,
@@ -104,26 +105,39 @@ class ColorData(Dataset):
         if name in codes.keys():
             return codes[name]
         
-    def make_data(self, data, paths, colors):
+    def make_data(self, data, paths, colors, transform):
+        print(data.keys())
+        '''
         for index, (key, value) in enumerate(data.items()):
             marker = key.find('\\')  # All this for class names
+            
             if marker == -1:
                 continue
             else:
                 color = key[:marker]
 
-            with open(f'{paths[index]}/key/{value}', 'rb') as f:
-                img = Image.open(f)
-                image = color_transform(img)
-                image = torch.tensor(image)
+            hi = data.keys()
+            idk = list(hi)[index]
 
-            name = self.encoding(color)
-            colors.append((image, name))
+            for file in value:
+                with open(f'{paths[index]}\\{key}\\{file}', 'rb') as f:
+                    img = Image.open(f)
+                    image = transform['train']
+                    image = image(img)
+                    image = torch.tensor(image)
 
-
+                    name = self.encoding(color)
+                    colors.append((image, name)) # Store as tuples (important)
+        '''
+        return colors
+    
     def split_data(colors):
         train, test = random_split(colors, [0.8, 0.2])
-
         return train, test
 
-            
+dict, paths = process_colors()    
+transform = color_transform()
+color = ColorData(dict, paths, transform)
+colors = color.make_data(dict, paths, [], transform)
+
+#train, test = color.split_data(colors)
